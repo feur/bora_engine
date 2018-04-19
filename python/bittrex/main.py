1cdfb297195359a15b8a44e89951ef5333f57a0d
Taras Woronjanski <tarascrysix@gmail.com>
2018-04-19 11:34:12 +1000

    addition of futher TA

--- python/bittrex/main.py
+++ python/bittrex/main.py
@@ -95,7 +95,7 @@ class MyPair(object):
             {u'C': 3.079e-05, u'H': 3.079e-05, u'L': 3.079e-05, u'O': 3.079e-05, u'BV': 0.04902765, u'T': u'2018-04-13T07:10:00', u'V': 1592.32422805}
             """
         account = Bittrex("f5d8f6b8b21c44548d2799044d3105f0", "b3845ea35176403bb530a31fd4481165", api_version=API_V2_0)
-        data = account.get_candles(entry.pair, tick_interval=TICKINTERVAL_FIVEMIN)
+        data = account.get_candles(entry.pair, tick_interval=TICKINTERVAL_HOUR)
         if (data['success'] == True):
             self.pairName = entry.pair
             self.data = data['result']
@@ -123,13 +123,10 @@ class MyPair(object):
             3. self.diPos[i] = (self.dmPosMax[i] / self.trMax[i]) * 100
     
         """
-        
-        #print("Di+ is %.9f" % diPos)
-        #print("Di- is %.9f" % diNeg)
-        #print("DAX is %.9f" % dax)
-        
+               
         self.diPos=[]
         self.diNeg=[]
+        self.tr=[]
         self.trMax=[]
         self.dmPos=[]
         self.dmNeg=[]
@@ -137,21 +134,60 @@ class MyPair(object):
         self.dmNegMax=[]
         
         
+        #calculate TR, dmPos, dmNeg
+        tr=[]
+        dmPos=[]
+        dmNeg=[]
+        
+        for i in range(1,len(self.data)):
+            itemP = self.data[i-1]
+            itemN = self.data[i]
+           
+            maximum = max(itemN['H'] - itemN['L'], abs(itemN['H'] - itemP['C']), abs(itemN['L'] - itemP['C']))
+            tr.append(maximum)
+            
+            if (itemN['H'] - itemP['H']) > (itemP['L'] - itemN['L']):
+                dmPos.append(max((itemN['H'] - itemP['H']), 0))
+            else:               
+                dmPos.append(0)
+                
+            if (itemP['L'] - itemN['L']) > (itemN['H'] - itemP['H']):
+                 dmNeg.append(max((itemP['L'] - itemN['L']), 0))               
+            else:
+                  dmNeg.append(0)
+               
+     
+        self.tr = tr
+        self.dmPos = dmPos
+        self.dmNeg = dmNeg
+    
         #calculate trMax,dmPosMax,dmNegMax
-        for i in range(0,13):
-             self.trMax[0] += self.tr[i]
-             self.dmPosMax[0] += self.dmPos[i]
-             self.dmNegMax[0] += self.dmNeg[i]
+        
+        SumTrMax = 0
+        SumdmPosMax = 0
+        SumdmNegMax = 0
+        
+        
+        for i in range(13):
+             SumTrMax += self.tr[i]
+             SumdmPosMax += self.dmPos[i]
+             SumdmNegMax += self.dmNeg[i]
+             
+        self.trMax.append(SumTrMax)
+        self.dmPosMax.append(SumdmPosMax)
+        self.dmNegMax.append(SumdmNegMax) 
+        
 
         for i in range(1,len(self.tr) - 13):
-            self.trMax[i] = self.trMax[i-1] - (self.trMax[i-1]/14) + self.tr[i+13]
-            self.dmPosMax[i] = self.dmPos[i-1] - (self.dmPosMax[i-1]/14) + self.dmPos[i+13]
-            self.dmNegMax[i] = self.dmNeg[i-1] - (self.dmPosMax[i-1]/14) + self.dmNeg[i+13]
-         
+            self.trMax.append(self.trMax[i-1] - (self.trMax[i-1]/14) + self.tr[i+13])
+            self.dmPosMax.append(self.dmPosMax[i-1] - (self.dmPosMax[i-1]/14) + self.dmPos[i+13])
+            self.dmNegMax.append(self.dmNegMax[i-1] - (self.dmNegMax[i-1]/14) + self.dmNeg[i+13])
+            
+
         #calculate diPos & diNeg
-        for i in range(0, len(self.trMax) - 1):
-            self.diPos[i] = (self.dmPosMax[i] / self.trMax[i]) * 100
-            self.diNeg[i] = (self.dmNegMax[i] / self.trMax[i]) * 100
+        for i in range(0, len(self.trMax)):
+            self.diPos.append((self.dmPosMax[i] / self.trMax[i]) * 100)
+            self.diNeg.append((self.dmNegMax[i] / self.trMax[i]) * 100)
             
               
         if (self.diNeg[-1] > 20) and (self.diNeg[-1] > self.diPos[-1]):
@@ -160,6 +196,9 @@ class MyPair(object):
             self.trend = 1  #no trend
         elif (self.diPos[-1] > 20) and (self.diPos[-1] > self.diNeg[-1]):
             self.trend = 2 #up trend
+            
+           
+    #    print("Trend: %d" % self.trend)   
       
 
     def GetSignal(self):
@@ -173,6 +212,13 @@ class MyPair(object):
         crossover = 1, self.trend = 0 , self.InCloud = 1 => self.signal = 1 [weak sell signal]
         crossover = 1, self.trend = 0 , self.InCloud = 1 => self.signal = 1 [weak sell signal]
         
+        
+        Initial: 
+            1. Find tenkanSen - (mean(high) + mean(low))/2 for 9 periods
+            2. find IchimokuAverage - (mean(high) + mean(low) / 2) for 26 periods
+            3. senkouA = (tenkanSen + kijunSen) / 2
+            4. senkouB - (mean(high) + mean(low) / 2)
+   
         
         1. Find crossover 
             Get SigTop where Pair = self.pairName
@@ -213,19 +259,110 @@ class MyPair(object):
                 
         """
         
-        tenkanSen = GetIchimokuAverage(GetFramedData(self.data, 9))
-        kijunSen = GetIchimokuAverage(GetFramedData(self.data, 26))
-        senkouA = (tenkanSen + kijunSen) / 2
-        senkouB = GetIchimokuAverage(GetFramedData(self.data, 52))
-
-        print(tenkanSen)
-        print(kijunSen)
-
-        if tenkanSen == kijunSen:
-            self.signal = 1
-        else:
-            self.signal = 0
+        high=[]
+        low=[]
+        highest=[]
+        lowest=[]
+        
+        self.tenkanSen = []
+        self.kijunSen = []
+        self.senkouB = []
+        self.senkouA = []
+        
+        for item in self.data:
+            high.append(item['H'])
+            low.append(item['L'])
             
+     
+        y = len(self.data)
+        
+        period = 9
+        
+        for x in range (y/period):  
+            
+            for z in range(y - (x+1)*period, y-(period*x + 1)):
+                highest.append(high[z])
+                lowest.append(low[z])
+                
+            self.tenkanSen.append((max(highest) + min(lowest))/2) 
+            highest *= 0
+            lowest *= 0
+                    
+         
+        period = 26
+        
+        for x in range (y/period):  
+            
+            for z in range(y - (x+1)*period, y-(period*x + 1)):
+                highest.append(high[z])
+                lowest.append(low[z])
+                
+            self.kijunSen.append((max(highest) + min(lowest))/2) 
+            highest *= 0
+            lowest *= 0
+                    
+       
+        period = 52
+        
+        for x in range (y/period):  
+            
+            for z in range(y - (x+1)*period, y-(period*x + 1)):
+                highest.append(high[z])
+                lowest.append(low[z])
+                
+            self.senkouB.append((max(highest) + min(lowest))/2) 
+            highest *= 0
+            lowest *= 0
+            
+            
+        period = 27
+        
+        for x in range(period-1):
+            self.senkouA.append((self.tenkanSen[x] + self.kijunSen[x])/ 2)
+            
+            
+       # print("red at: %.9f" % self.tenkanSen[0])  
+       # print("blue at: %.9f" % self.kijunSen[0])  
+        
+        #find crossover 
+        if (self.tenkanSen[1] > self.kijunSen[1]) and (self.tenkanSen[0] < self.kijunSen[0]):
+            self.crossover = 1
+        elif (self.tenkanSen[1] < self.kijunSen[1]) and (self.tenkanSen[0] > self.kijunSen[0]):
+            self.crossover = 1    
+        elif (self.tenkanSen[0] == self.kijunSen[0]): 
+            self.crossover = 1 
+        else:
+            self.crossover = 0
+            
+     #   print("crossover: %d" % self.crossover)    
+        
+        
+        #find incloud
+        
+        if ((self.senkouA[0] > self.senkouB[0]) and (self.current['C'] < self.senkouA[0])):
+            self.InCloud = 1
+        elif ((self.senkouB[0] > self.senkouA[0]) and (self.current['C'] < self.senkouB[0])):
+            self.InCloud = 1
+        else:
+            self.InCloud = 0
+                    
+      #  print("cloud: %d"% self.InCloud)
+        
+        if self.crossover == 0 or self.trend == 1:
+            self.signal = 2 #no signal
+        elif self.trend == 0 and self.InCloud == 0 and self.crossover == 1:
+            self.signal = 0 #strong sell
+        elif self.trend == 0 and self.InCloud == 0 and self.crossover == 1:
+            self.signal = 1 #weak sell    
+        elif self.trend == 2 and self.InCloud == 1 and self.crossover == 1:
+            self.signal = 3 #weak buy
+        elif self.trend == 2 and self.InCloud == 0 and self.crossover == 1:
+            self.signal = 4 #strong buy
+       
+            
+        print("signal: %d"% self.signal)    
+                
+      
             
         
     def GetRating(self):
@@ -233,7 +370,13 @@ class MyPair(object):
         
         self.momentum = GetMomentum(self.data)        
         self.fib = GetFib(self.current['C'], entry.a, entry.b, entry.c, entry.d, entry.e, entry.f)
-        self.rating = self.fib * self.momentum
+
+     
+        self.rating = self.fib * (self.diPos[-1] + self.diNeg[-1]) * self.momentum
+        
+        print(self.rating)
+
+
 
 
 
@@ -242,21 +385,18 @@ class MyPair(object):
 entry = GetEntry() ##Get arguments to define Pair and Fib levels
 pid = os.getpid()  ##Get process pid
 
+print("pid is: %d" % pid)
+
 while True:  ##Forever loop 
 
     pair = MyPair(entry)
 
-
-    print(pid)
-
     print("current price is: %.9f" % pair.current['C'])
+    
+    
+    pair.GetTrend()
     pair.GetSignal()
     pair.GetRating()
-
-    print(pair.signal)
-    print(pair.fib)
-    print(pair.momentum)
-    print(pair.rating)
 
 
 
--- python/bittrex/ta.py
+++ python/bittrex/ta.py
@@ -25,71 +25,7 @@ def GetFib(current, a, b, c, d, e,f):
     fib = (current - low) / (high - low)
     return fib
     
-def GetTr(data):
-    tr=[]
 
-    for i in range(1,28):
-       itemP = data[i-1]
-       itemN = data[i]
-       maximum = max(abs(itemN['H'] - itemN['L']), abs(itemN['H'] - itemN['C']), abs(itemN['L'] - itemP['C']))
-       tr.append(maximum)
-
-    print(tr)
-    return tr
-    
-def GetTrMax(tr):
-    trMax=[]
-
-    dis = 14
-    sum = 0
-
-    for i in range (0, dis):
-        sum += tr[i]
-
-    trMax.append(sum)
-
-    for i in range(1,dis):
-        trMax.append(trMax[i-1] - (trMax[i-1]/14) + tr[i+dis])
-
-    print(len(trMax))
-    return trMax
-    
-def GetdmPos(data):
-    dmPos=[]
-
-    for i in range(1,28):
-        itemP = data[i-1]
-        itemN = data[i]
-
-    if (itemN['H'] - itemP['H']) > (itemP['L'] - itemN['L']):
-        dmPos.append(0)
-    else:
-        dmPos.append(max((itemN['H'] - itemP['H']), 0))
-
-    print(dmPos)
-    return dmPos
-    
-def GetdmNeg(data):
-    dmNeg=[]
-    for i in range(1,28):
-        itemP = data[i-1]
-        itemN = data[i]
-
-    if (itemP['L'] - itemN['L']) > (itemN['H'] - itemN['H']):
-        dmNeg.append(0)
-    else:
-        dmNeg.append(max((itemP['L'] - itemN['L']), 0))
-
-    print(dmNeg)
-    return dmNeg
-    
-def GetdiPos(tr,dmPos,dmNeg):
-    trMax = sum(tr)
-    diPos = 100 * (dmPos/trMax)
-    return diPos
-
-#def GetdiNeg(data):
- #   return diNeg
 
 def GetDX(diPos, diNeg):
     diDiff = abs(diPos - diNeg)
@@ -104,7 +40,7 @@ def GetADX(dx):
 def GetMomentum(data):
     current = data[-1]
     previous = data[-11]
-    momentum = (current['C'] / previous['C'] - 1)
+    momentum = (current['C'] / previous['C'] * 100)
     return momentum
     
 def GetIchimokuAverage(data):
