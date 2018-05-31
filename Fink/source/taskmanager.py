5ffa3c87ee435de3dc1736280a5be2647d6ee8d5
Taras Woronjanski <tarascrysix@gmail.com>
2018-05-31 13:24:55 +1000

    cleaning up code

--- Fink/source/taskmanager.py
+++ Fink/source/taskmanager.py
@@ -10,7 +10,7 @@ from settings import *
 
 
             
-def StartAllUnits(conn, listofpairs):
+def StartAllUnits(conn):
     
     cursor = conn.cursor()
     
@@ -32,63 +32,42 @@ def StartAllUnits(conn, listofpairs):
         conn.close()
         
         
-def StartAgent(pair,conn):
-        
-    ##making sure all agents run
-        cursor = conn.cursor()
-        query = "SELECT PID from Pairs WHERE Pair = '%s'" % (pair)
-        
-        try:
-            cursor.execute (query)
-            data = cursor.fetchone() #find PID for PAIR
+def StartAgents(conn):
+    
+    ##get a list of all Pairs in database and find it's pid and check it        
+    cursor = conn.cursor()
+  
+    try:
+        cursor.execute ("SELECT `Pair`, `PID` FROM `Pairs` WHERE 1")
+        data = cursor.fetchall()
+
+        for i in range(len(data)):
             
-            #check if pid is running
-            if psutil.pid_exists(data[0]):
-                print("agent for %s is still running with pid %d" % (pair, data[0]))
+            pair = str(data[i][0])
+            
+            if psutil.pid_exists(data[i][1]):
+                print("agent for %s is still running with pid %d" % (pair, data[i][1]))
             else:
                 print("re-running agent for this pair %s" % (pair))
-                process = subprocess.call("python ~/Fink/source/agent.py " + "-p " + pair + " > /dev/null 2>&1 & ",  shell=True)
-               
-        except MySQLdb.Error as error:
-            print(error)
-            conn.close()
+                agent = subprocess.call("python ~/Fink/source/agent.py " + "-p " + pair + " > /dev/null 2>&1 & ",  shell=True)
+   
+    except MySQLdb.Error as error:
+        print(error)
+        conn.close()
+              
         
-                
-
-    
+        
 ##program start here
     
-ListofPairs = []   ##list of Pairs, e.g. BTC-ADA, ETH-ADA
-ListofCurrencies= [] ##list of Currencies e.g. ADA, OMG
-
 pid = os.getpid()               ##Get process pid
 print("pid is: %d" % pid)
 
 conn = MySQLdb.connect(Fink_DB_HOST,Fink_DB_USER,Fink_DB_PW,Fink_DB_NAME) ##connect to DB
 
-
-##get a list of all Pairs in database        
-cursor = conn.cursor()
-  
-try:
-    cursor.execute ("SELECT `Pair`, `Currency` FROM `Pairs` WHERE 1")
-    data = cursor.fetchall()
-    
-    for i in range(len(data)):
-        ListofPairs.append(str(data[i][0]))
-        ListofCurrencies.append(str(data[i][1]))
-          
-except MySQLdb.Error as error:
-    print(error)
-    conn.close()
-    
-
-
 while True: 
     
     StartAllUnits(conn) ##start all components and agents
-    for i in range(len(ListofPairs)):
-        StartAgent(ListofPairs[i],conn)
+    StartAgents(conn)
     time.sleep(10)
     
    
