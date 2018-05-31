0893d2c876e50fa603dddb163e55ba783835913f
Taras Woronjanski <tarascrysix@gmail.com>
2018-05-31 18:03:32 +1000

    restored taskmanager

--- Fink/source/taskmanager.py
+++ Fink/source/taskmanager.py
@@ -10,8 +10,9 @@ from settings import *
 
 
             
-def StartAllUnits(conn):
+def StartAllUnits():
     
+    conn = MySQLdb.connect(Fink_DB_HOST,Fink_DB_USER,Fink_DB_PW,Fink_DB_NAME) ##connect to DB
     cursor = conn.cursor()
     
     try:       
@@ -24,50 +25,25 @@ def StartAllUnits(conn):
             if psutil.pid_exists(data[i][1]):
                 print("process %s is still running with pid %d" % (data[i][0], data[i][1]))
             else:
-                print("re-running process for this component %s" % (data[i][0]))
-                process = subprocess.call("python ~/Fink/source/" + data[i][0] + ".py > /dev/null 2>&1 & ",  shell=True)
+                    print("re-running process for this component %s because PID %s doesn't exist" % (data[i][0],data[i][1]))
+                    process = subprocess.call("python ~/Fink/source/" + data[i][0] + ".py > /dev/null 2>&1 & ",  shell=True)
               
     except MySQLdb.Error as error:
         print(error)
-        conn.close()
+        conn.close()   
         
-        
-def StartAgents(conn):
-    
-    ##get a list of all Pairs in database and find it's pid and check it        
-    cursor = conn.cursor()
-  
-    try:
-        cursor.execute ("SELECT `Pair`, `PID` FROM `Pairs` WHERE 1")
-        data = cursor.fetchall()
+                
 
-        for i in range(len(data)):
-            
-            pair = str(data[i][0])
-            
-            if psutil.pid_exists(data[i][1]):
-                print("agent for %s is still running with pid %d" % (pair, data[i][1]))
-            else:
-                print("Agent with PID: %s is not runnign, re-running agent for this pair %s" % (data[i][1],pair))
-                agent = subprocess.call("python ~/Fink/source/agent.py " + "-p " + pair + " > /dev/null 2>&1 & ",  shell=True)
-   
-    except MySQLdb.Error as error:
-        print(error)
-        conn.close()
-              
-        
-        
-##program start here
     
+##program start here
+
 pid = os.getpid()               ##Get process pid
 print("pid is: %d" % pid)
 
-conn = MySQLdb.connect(Fink_DB_HOST,Fink_DB_USER,Fink_DB_PW,Fink_DB_NAME) ##connect to DB
 
 while True: 
     
-    StartAllUnits(conn) ##start all components and agents
-    StartAgents(conn)
+    StartAllUnits()
     time.sleep(10)
     
    
