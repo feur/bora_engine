82c0a2e6ba6cb103c18c3953930248e8b460a7a4
TarasWoronjanski <taras.woronjanski@ammo.co>
2018-06-11 05:29:04 +0000

    modified agent

--- Fink/source/settings.py
+++ Fink/source/settings.py
@@ -3,22 +3,17 @@ from decimal import Decimal, getcontext
 
 getcontext().prec = 8
 
-#LOCAL DB#
-#DB_NAME = "aurade"
-#DB_HOST = "localhost"
-#3DB_PW = "Amm02o16!"
-#DB_USER = "root"
 
 #Fink DB#
 Fink_DB_NAME = "Fink"
-Fink_DB_HOST = "138.197.194.3"
-Fink_DB_PW = "5AKC08noMTwx9lG2"
-Fink_DB_USER = "dev"
+Fink_DB_HOST = "localhost"
+Fink_DB_PW = "Amm02o16!"
+Fink_DB_USER = "root"
 
 #Edel DB#
 Edel_DB_NAME = "Edel"
 Edel_DB_HOST = "138.197.194.3"
-Edel_DB_PW = "5AKC08noMTwx9lG2"
-Edel_DB_USER = "dev"
+Edel_DB_PW = "QkK9GTOQuia5DjzC"
+Edel_DB_USER = "user"
     
 ##QkK9GTOQuia5DjzC
