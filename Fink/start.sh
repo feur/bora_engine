68d70cd881bb15de554d768e93158010c6aab2ea
Taras Woronjanski <tarascrysix@gmail.com>
2018-07-03 01:05:19 +1000

    .sql included

--- Fink/start.sh
+++ Fink/start.sh
@@ -2,4 +2,4 @@
 
 killall python
 
-~/Fink/source/manager/manager -k 5e11284c284449beb4746c5140b8e2af -s f19ae69dc7a1457db23419e935bb07e3 -u JJ433KL -t 5 -m 0.985 -n 1.02 -d 1.005 -l 0.02 -f 0.2 -ex 1 -st 1 
+~/Fink/source/manager/manager -k f5d8f6b8b21c44548d2799044d3105f0 -s b3845ea35176403bb530a31fd4481165 -u FHETUK4 -t 5 -m 0.985 -n 1.02 -d 1.005 -l 0.007 -f 0.2 -ex 0 -st 1 
