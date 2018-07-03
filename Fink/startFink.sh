4a6a090474d1d83fb73545b83c24722d9ac15d85
Taras Woronjanski <tarascrysix@gmail.com>
2018-07-03 14:55:11 +1000

    found a bug

--- Fink/startFink.sh
+++ Fink/startFink.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-killall python
+#killall python
 
-~/Fink/source/fink/fink -p BTC-FCT -k 5e11284c284449beb4746c5140b8e2af -s f19ae69dc7a1457db23419e935bb07e3 -u JJ433KL -t 1 -m 0.985 -n 1.005 -d 1.005 -l 0.02 -f 0.2 -ex 1 -st 1 
+#~/Fink/source/fink/fink -p BTC-FCT -k 5e11284c284449beb4746c5140b8e2af -s f19ae69dc7a1457db23419e935bb07e3 -u JJ433KL -t 1 -m 0.985 -n 1.005 -d 1.005 -l 0.02 -f 0.2 -ex 1 -st 1 
+~/Fink/source/fink/fink -p BTC-SPHR -k f5d8f6b8b21c44548d2799044d3105f0 -s b3845ea35176403bb530a31fd4481165 -u FHETUK4 -t 5 -m 0.99 -n 1.02 -d 1.005 -l 0.035 -f 0.2 -ex 0 -st 1 
+#-t 5 -m 0.99 -n 1.02 -d 1.005 -l 0.035 -f 0.2 -ex 0 -st 1 
+
+
+
+
+
