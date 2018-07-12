013038f06f53051d711572a512a7087a6688803b
Taras Woronjanski <tarascrysix@gmail.com>
2018-07-13 03:07:23 +1000

    adjusted params

--- FINK/fink_full/start.sh
+++ FINK/fink_full/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-~/Fink/source/manager/manager -k f5d8f6b8b21c44548d2799044d3105f0 -s b3845ea35176403bb530a31fd4481165 -u FHETUK4 -t 5 -e 10 -m 1 -n 1 -d 0.993 -l 0.01 -ex 0 
+~/Fink/source/manager/manager -k f5d8f6b8b21c44548d2799044d3105f0 -s b3845ea35176403bb530a31fd4481165 -u FHETUK4 -t 5 -e 50 -m 0.995 -n 1 -d 0.995 -l 0.01 -ex 0 
