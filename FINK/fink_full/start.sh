2214b88cdc68f0858841943fe14dbf32ff1da86b
Taras Woronjanski <tarascrysix@gmail.com>
2018-07-15 19:49:19 +1000

    fixed RMI

--- FINK/fink_full/start.sh
+++ FINK/fink_full/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-~/Fink/source/manager/manager -k f5d8f6b8b21c44548d2799044d3105f0 -s b3845ea35176403bb530a31fd4481165 -u FHETUK4 -t 5 -st 0.98 -m 1 -n 1 -l 0.04 -ex 0 
+~/Fink/source/manager/manager -k f5d8f6b8b21c44548d2799044d3105f0 -s b3845ea35176403bb530a31fd4481165 -u FHETUK4 -t 5 -sl 0.98 -m 1 -n 1 -l 0.04 -ex 0 
