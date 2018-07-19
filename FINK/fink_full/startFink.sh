ed42447a57eac2829b3fbd8051240d079fe01658
Taras Woronjanski <tarascrysix@gmail.com>
2018-07-19 14:10:45 +1000

    complete new strat

--- FINK/fink_full/startFink.sh
+++ FINK/fink_full/startFink.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-~/Fink/source/fink/fink -p BTC-ADA -k f5d8f6b8b21c44548d2799044d3105f0 -s b3845ea35176403bb530a31fd4481165 -u FHETUK4 -t 30 -e 10 -sl 0.99 -m 1 -n 1 -l 1 -ex 0
-
-
+~/Fink/source/fink/fink -p BTC-TRX -k f5d8f6b8b21c44548d2799044d3105f0 -s b3845ea35176403bb530a31fd4481165 -u FHETUK4 -t 5 -e 10 -t 0.005 -l 1 -ex 0
