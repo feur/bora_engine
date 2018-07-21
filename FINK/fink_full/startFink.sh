77bccb37289d1645cff8949b4f9454ee6544da04
Taras Woronjanski <tarascrysix@gmail.com>
2018-07-21 19:19:25 +1000

    smarter Fink

--- FINK/fink_full/startFink.sh
+++ FINK/fink_full/startFink.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-~/Fink/source/fink/fink -p BTC-TRX -k f5d8f6b8b21c44548d2799044d3105f0 -s b3845ea35176403bb530a31fd4481165 -u FHETUK4 -t 5 -e 10 -t 0.005 -l 1 -ex 0
+~/Fink/source/fink/fink -p BTC-ADA -k f5d8f6b8b21c44548d2799044d3105f0 -s b3845ea35176403bb530a31fd4481165 -u FHETUK4 -t 5 -e 10 -l 1
