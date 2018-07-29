85d4631bb26baa7d005a6d5df43bcd6890d4454d
Taras Woronjanski <tarascrysix@gmail.com>
2018-07-29 18:47:24 +1000

    automatic cycling

--- FINK/fink_full/startFink.sh
+++ FINK/fink_full/startFink.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-~/Fink/source/fink/fink -p BTC-ADA -k f5d8f6b8b21c44548d2799044d3105f0 -s b3845ea35176403bb530a31fd4481165 -u FHETUK4 -t 5 -e 10 -l 1
+~/Fink/source/fink/fink -p BTC-GLD -k f5d8f6b8b21c44548d2799044d3105f0 -s b3845ea35176403bb530a31fd4481165 -u FHETUK4 -t 5 -e 10 -l 1 -a 1 -c 6
