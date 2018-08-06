b5cbf89b605874b7cdccf0f46960633761adca2a
Taras Woronjanski <tarascrysix@gmail.com>
2018-08-06 19:32:42 +1000

    tuned 1 min

--- FINK/fink_full/startFink.sh
+++ FINK/fink_full/startFink.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-~/Fink/source/fink/fink -p BTC-GLD -k f5d8f6b8b21c44548d2799044d3105f0 -s b3845ea35176403bb530a31fd4481165 -u FHETUK4 -t 5 -e 10 -l 1 -a 1 -c 6
+~/Fink/source/fink/fink -p BTC-CURE -k f5d8f6b8b21c44548d2799044d3105f0 -s b3845ea35176403bb530a31fd4481165 -u FHETUK4 -t 1 -a 1 -c 6 -l 0.012 -sl 0.9 -rl 1.01 -fw 120 -lp 1440
+
