e47213a0b29660278d5d3369eda0a68b44ffe9e2
Taras <taras.woronjanski@ammo.co>
2018-08-08 20:37:38 +1000

    ...

--- FINK/fink_full/startFink.sh
+++ FINK/fink_full/startFink.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-~/Fink/source/fink/fink -p BTC-ADA -k f5d8f6b8b21c44548d2799044d3105f0 -s b3845ea35176403bb530a31fd4481165 -u FHETUK4 -t 5 -a 0 -c 4 -l 0.012 -sl 0.9 -rl 1.01 -lp 288
+~/Fink/source/fink/fink -p BTC-XMR -k f5d8f6b8b21c44548d2799044d3105f0 -s b3845ea35176403bb530a31fd4481165 -u FHETUK4 -t 5 -a 0 -c 4 -l 0.0055 -sl 0.9 -rl 1.01 -lp 864
 
