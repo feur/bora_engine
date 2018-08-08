08faf7203e65f46701ba60e1ccc4b521ea36c78a
Taras <taras.woronjanski@ammo.co>
2018-08-08 16:44:22 +1000

    ...

--- FINK/fink_full/start.sh
+++ FINK/fink_full/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-~/Fink/source/manager/manager -k f5d8f6b8b21c44548d2799044d3105f0 -s b3845ea35176403bb530a31fd4481165 -u FHETUK4 -t 5 -a 0 -c 4 -l 0.012 -sl 0.9 -rl 1.01 -lp 864
+~/Fink/source/manager/manager -k f5d8f6b8b21c44548d2799044d3105f0 -s b3845ea35176403bb530a31fd4481165 -u FHETUK4 -t 5 -a 0 -c 4 -l 0.0055 -sl 0.9 -rl 1.01 -lp 864
