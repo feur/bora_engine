f376b2f069ad51f52c1f4b6d71e0a4329971dc42
Taras <taras.woronjanski@ammo.co>
2018-08-09 00:54:56 +1000

    ...

--- FINK/fink_full/startFink.sh
+++ FINK/fink_full/startFink.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-~/Fink/source/fink/fink -p BTC-XVG -k f5d8f6b8b21c44548d2799044d3105f0 -s b3845ea35176403bb530a31fd4481165 -u FHETUK4 -t 5 -a 0 -c 4 -l 1 -sl 0.9 -rl 1.01 -lp 4032
+~/Fink/source/fink/fink -p BTC-XVG -k f5d8f6b8b21c44548d2799044d3105f0 -s b3845ea35176403bb530a31fd4481165 -u FHETUK4 -t 5 -a 0 -l 1 -sl 0.9 -rl 1.01 -lp 288
 
 
 
