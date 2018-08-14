127be813ce85ba6024b478b51fae496d5a9d2a84
Taras <taras.woronjanski@ammo.co>
2018-08-14 21:59:58 +1000

    allow maintaining order when holding something

--- FINK/fink_full/startFink.sh
+++ FINK/fink_full/startFink.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-~/Fink/source/fink/fink -p BTC-HMQ -k f5d8f6b8b21c44548d2799044d3105f0 -s b3845ea35176403bb530a31fd4481165 -u FHETUK4 -t 6 -a 2 -l 0.025 -sl 0.98 -lp 720
+~/Fink/source/fink/fink -p BTC-DYN -k f5d8f6b8b21c44548d2799044d3105f0 -s b3845ea35176403bb530a31fd4481165 -u FHETUK4 -t 6 -a 2 -l 0.025 -sl 0.98 -lp 720
 
 
 
