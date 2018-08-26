85c2102e14e2fe742c6d43dbeb696eb42e50256c
feur <crysixoner@gmail.com>
2018-08-26 15:17:19 +1000

    optimized code, reduced optimization time, included optimized stop loss

--- x86/FINK/startFink.sh
+++ x86/FINK/startFink.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-~/Fink/source/fink/fink -k f5d8f6b8b21c44548d2799044d3105f0 -s b3845ea35176403bb530a31fd4481165 -u FHETUK4 -t 60 -a 0 -l 1 -sl 0.96 -lp 1400 -ex 0 -st 1 -p BTC-GTO -c GTO
+~/Fink/source/fink/fink -k f5d8f6b8b21c44548d2799044d3105f0 -s b3845ea35176403bb530a31fd4481165 -u FHETUK4 -t 60 -a 0 -l 1 -lp 1400 -ex 0 -st 1 -p BTC-GTO -c GTO
 
 
 
