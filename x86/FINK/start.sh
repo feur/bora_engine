65e28e485817ea196e484b3e65ac1f3d6b7c3d00
feur <crysixoner@gmail.com>
2018-09-11 02:30:02 +1000

    ...

--- x86/FINK/start.sh
+++ x86/FINK/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-~/Fink/source/manager/manager -k f5d8f6b8b21c44548d2799044d3105f0 -s b3845ea35176403bb530a31fd4481165 -u FHETUK4 -t 5 -l 0.14 -ex 0 -st 0 -id 0 -ip localhost
+python ~/Fink/source/manager.py -k f5d8f6b8b21c44548d2799044d3105f0 -s b3845ea35176403bb530a31fd4481165 -u FHETUK4 -t 5 -l 0.14 -ex 0 -st 0 -id 0 -ip localhost
--- x86/FINK/startFink.sh
+++ x86/FINK/startFink.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-~/Fink/source/fink/fink -k f5d8f6b8b21c44548d2799044d3105f0 -s b3845ea35176403bb530a31fd4481165 -u FHETUK4 -t 5 -l 1 -ex 0 -st 1 -p BTC-LTC -c LTC
+python ~/Fink/source/fink.py -k f5d8f6b8b21c44548d2799044d3105f0 -s b3845ea35176403bb530a31fd4481165 -u FHETUK4 -t 5 -l 1 -ex 0 -st 1 -p BTC-LTC -c LTC
 
 
 
